d99902b3b8456af021a1fbd00f6d80bbcaa0a0d5
James <james@team-wombat.com>
2018-02-23 07:52:47 -0700

    Switched profile pop to user, updates during sitter flow. Must do same to pet reg flow and change to user rootscope.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :lastname, :phone1])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :lastname, :phone1, :address1, :address2, :city, :state, :zipcode])
   end
 
     # prepend_before_action :configure_permitted_parameters, if: :devise_controller?
